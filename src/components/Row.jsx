fc9cfc55ba4cbaf4e3863fd668e79bc7a0d8ed7a
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2021-03-22 09:07:35 +0300

    Row component

---
+++ src/components/Row.jsx
@@ -0,0 +1,46 @@
+import axios from "../axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+  const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h3>{title}</h3>
+      <div className="row__posters">
+        {movies.map((movie) => {
+          return (
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
